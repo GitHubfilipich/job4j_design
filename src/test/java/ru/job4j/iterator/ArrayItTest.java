99d0d3cc925b7735d43ce62daa0d4640610f6cb2
YuryGe <postbox177@yandex.ru>
2023-08-13 16:47:00 +0400

    1. Что такое итератор. [#4951] exmpls

--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -2,27 +2,36 @@ package ru.job4j.iterator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ArrayItTest {
 
     @Test
-    void whenMultiCallHasNextThenTrue() {
+    public void whenMultiCallHasNextThenTrue() {
         ArrayIt iterator = new ArrayIt(
                 new int[] {1, 2, 3}
         );
-        boolean result = iterator.hasNext();
-        assertThat(result).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
         assertThat(iterator.hasNext()).isTrue();
     }
 
     @Test
-    void whenReadSequence() {
+    public void whenReadSequence() {
         ArrayIt iterator = new ArrayIt(
                 new int[] {1, 2, 3}
         );
         assertThat(iterator.next()).isEqualTo(1);
         assertThat(iterator.next()).isEqualTo(2);
         assertThat(iterator.next()).isEqualTo(3);
+    }
+
+    @Test
+    public void whenNextFromEmpty() {
+        ArrayIt iterator = new ArrayIt(
+                new int[] {}
+        );
+        assertThatThrownBy(iterator::next).isInstanceOf(NoSuchElementException.class);
     }
 }
