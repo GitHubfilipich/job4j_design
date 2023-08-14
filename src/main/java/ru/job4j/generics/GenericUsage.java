f2f54c3dc41eba1910650cff3faae8974b51d039
YuryGe <postbox177@yandex.ru>
2023-08-14 22:33:26 +0400

    1. Что такое обобщенные типы (generics) [#4952] исправление

--- src/main/java/ru/job4j/generics/GenericUsage.java
+++ src/main/java/ru/job4j/generics/GenericUsage.java
@@ -4,15 +4,6 @@ import java.util.*;
 
 public class GenericUsage {
     public static void main(String[] args) {
-//        List<Integer> list = List.of(1, 2, 3, 4, 5);
-//        new GenericUsage().printRsl(list);
-
-//        List<Person> per = List.of(new Person("name", 21, new Date(913716000000L)));
-//        new GenericUsage().printInfo(per);
-//
-//        List<Programmer> pro = List.of(new Programmer("name123", 23, new Date(913716000000L)));
-//        new GenericUsage().printInfo(pro);
-
         List<? super Integer> list = new ArrayList<>();
         new GenericUsage().addAll(list);
     }
