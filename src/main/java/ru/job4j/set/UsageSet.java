b21ae855ba2645919a4df1770265b0493e392f41
YuryGe <postbox177@yandex.ru>
2023-08-29 12:09:46 +0400

    0. Set, HashSet, TreeSet [#4954 #440613]

---
+++ src/main/java/ru/job4j/set/UsageSet.java
@@ -0,0 +1,26 @@
+package ru.job4j.set;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class UsageSet {
+    public static void main(String[] args) {
+        Set<String> strings = new HashSet<>();
+        strings.add("one");
+        strings.add("two");
+        strings.add("three");
+        strings.add("three");
+        Iterator<String> it = strings.iterator();
+        while (it.hasNext()) {
+            System.out.println("Текущий элемент: " + it.next());
+        }
+        boolean b = strings.contains("two");
+        System.out.println("Множество содержит элемент: " + b);
+        int size = strings.size();
+        System.out.println("Наше множество содержит: " + size + " элемента.");
+        strings.stream()
+                .filter(s -> s.length() < 5)
+                .forEach(st -> System.out.println("Текущий элемент: " + st));
+    }
+}
