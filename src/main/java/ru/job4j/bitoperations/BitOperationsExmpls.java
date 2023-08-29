5583460f79655fd44271ca46250f5cd34da55b95
YuryGe <postbox177@yandex.ru>
2023-08-29 15:22:28 +0400

    1. Побитовые операции [#504964] - exmpls

---
+++ src/main/java/ru/job4j/bitoperations/BitOperationsExmpls.java
@@ -0,0 +1,9 @@
+package ru.job4j.bitoperations;
+
+public class BitOperationsExmpls {
+
+    public static void main(String[] args) {
+        int d1 = -5;
+        System.out.println("Decimal " + d1 + " = " + Integer.toBinaryString(d1));
+    }
+}
