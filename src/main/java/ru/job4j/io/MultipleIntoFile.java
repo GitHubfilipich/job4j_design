572d6a4f20147e1747504745833021e147f88273
YuryGe <postbox177@yandex.ru>
2023-11-07 22:06:21 +0400

    2.0. FileOutputStream [#252488 #458364] исправления 2

--- src/main/java/ru/job4j/io/MultipleIntoFile.java
+++ src/main/java/ru/job4j/io/MultipleIntoFile.java
@@ -7,18 +7,10 @@ public class MultipleIntoFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/fileofmultiple.txt")) {
             for (int i = 1; i <= 9; i++) {
-                try {
-                    out.write(String.format("1 * %1$s = %1$s", i).getBytes());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    out.write(System.lineSeparator().getBytes());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                out.write(String.format("1 * %1$s = %1$s", i).getBytes());
+                out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
