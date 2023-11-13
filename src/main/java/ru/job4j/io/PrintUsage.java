947de09c8950a09190944effbfe07128a31befd6
YuryGe <postbox177@yandex.ru>
2023-11-13 10:35:58 +0400

    5.1. BufferedReader [#252489 #459534]  - исправления

--- src/main/java/ru/job4j/io/PrintUsage.java
+++ src/main/java/ru/job4j/io/PrintUsage.java
@@ -4,18 +4,6 @@ import java.io.*;
 
 public class PrintUsage {
     public static void main(String[] args) {
-//        try (PrintStream stream = new PrintStream(new FileOutputStream("data/print.txt"))) {
-//        try (PrintStream stream = new PrintStream("data/print.txt")) {
-//        File file = new File("data/print.txt");
-//        try (PrintStream stream = new PrintStream(file)) {
-//            stream.println("Из PrintStream в FileOutputStream3");
-//        try (PrintStream stream = new PrintStream(new FileOutputStream("data/print.txt"))) {
-////        try (FileOutputStream stream = new FileOutputStream("data/print.txt")) {
-//            stream.println("Из PrintStream в FileOutputStream4");
-//            stream.write("Новая строка".getBytes());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         try (PrintStream stream = new PrintStream(new FileOutputStream("data/print.txt"));
              PrintWriter writer = new PrintWriter("data/write.txt")) {
             stream.println("Из PrintStream в FileOutputStream 5");
