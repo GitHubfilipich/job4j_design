7f606bb1fd7bae433b54524036f19a18a405a14e
YuryGe <postbox177@yandex.ru>
2023-11-14 09:23:22 +0400

    5.2. BufferedOutputStream [#252490] xmpls

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,12 +1,23 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
             out.write("Hello, world!".getBytes());
             out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/result.txt")
+                ))) {
+            out.println("Hello, world!");
+            out.printf("%s%n%s", "Hello", "world");
         } catch (Exception e) {
             e.printStackTrace();
         }
