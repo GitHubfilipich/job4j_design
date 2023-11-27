3630e48cfb74be79c9cf39b4ec5f3aa37a0bcd50
YuryGe <postbox177@yandex.ru>
2023-11-27 09:54:03 +0400

    15.1. Регулярные выражения. Метасимволы, квантификаторы, классы символов [#504918 #462758]  - xmpls

--- src/main/java/ru/job4j/regex/RegexExample.java
+++ src/main/java/ru/job4j/regex/RegexExample.java
@@ -1,5 +1,6 @@
 package ru.job4j.regex;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,5 +19,30 @@ public class RegexExample {
         matcher = pattern.matcher(text);
         String rsl = matcher.replaceAll("Job4j");
         System.out.println(rsl);
+
+        pattern = Pattern.compile("11");
+        text = "111111";
+        matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+
+        String str = "123+=-456:/789";
+        String[] rsl1 = str.split("\\D+");
+        System.out.println(Arrays.toString(rsl1));
+
+        pattern = Pattern.compile("\\b\\d{2}\\.\\d{2}\\.\\d{4}\\b");
+        text = "24.04.1987 11.11.111111 99.99.99991 99.99.9999 99999999 0000.00.00";
+        matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+
+        pattern = Pattern.compile("\\S{1,}@\\S{1,}\\.\\S{1,}");
+        text = "peter-2022@example.com example65@mail_box.ru 123_45@example-mailbox.com";
+        matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
     }
 }
