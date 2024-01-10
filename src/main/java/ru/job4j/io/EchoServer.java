deff7edc86ac892b66d5386bb912ceff7d1539a3
YuryGe <postbox177@yandex.ru>
2024-01-10 10:07:03 +0400

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,9 +3,12 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        Pattern pattern = Pattern.compile("msg=.+ ");
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -13,11 +16,19 @@ public class EchoServer {
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (input.readLine().contains("/?msg=Bye ")) {
-                        server.close();
-                    }
-                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        System.out.println(string);
+                    String string = input.readLine();
+                    if (string != null && !string.isEmpty()) {
+                        Matcher matcher = pattern.matcher(string);
+                        if (matcher.find()) {
+                            switch (matcher.group()) {
+                                case "msg=Hello " -> output.write("Hello".getBytes());
+                                case "msg=Exit " -> {
+                                    output.write("Server closed".getBytes());
+                                    server.close();
+                                }
+                                default -> output.write("What".getBytes());
+                            }
+                        }
                     }
                     output.flush();
                 }
