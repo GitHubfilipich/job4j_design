b2fd08fc87e0960eb909f260fd14da9281d0965b
YuryGe <postbox177@yandex.ru>
2024-01-12 08:50:04 +0400

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,7 +10,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         Pattern pattern = Pattern.compile("msg=.+ ");
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -33,6 +39,8 @@ public class EchoServer {
                     output.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Exception in main method: ", e);
         }
     }
 }
