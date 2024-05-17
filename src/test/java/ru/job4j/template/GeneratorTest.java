c7d28950606c0ceaed12b2462a9ec785a862ad6a
YuryGe <postbox177@yandex.ru>
2024-05-17 22:46:13 +0400

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GeneratorTest {
+
+    @Test
+    void whenCorrectParametersThenProduceCorrectResult() {
+        Generator generator = (template, args) -> null;
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
+        String result = "I am a Petr Arsentev, Who are you?";
+        assertThat(generator.produce(template, args)).isEqualTo(result);
+    }
+
+    @Test
+    void whenTooMuchParametersInTemplateThenGetException() {
+        Generator generator = (template, args) -> null;
+        String template = "I am a ${name}, I am ${age} years old, Who are ${subject}";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you");
+        assertThatThrownBy(() -> generator.produce(template, args))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenTooMuchEntriesInArgsThenGetException() {
+        Generator generator = (template, args) -> null;
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = Map.of("name", "Petr Arsentev", "subject", "you", "age", "30");
+        assertThatThrownBy(() -> generator.produce(template, args))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
