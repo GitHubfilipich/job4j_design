e250e57cff8f95992cb5bff7413dd855385c82b4
YuryGe <postbox177@yandex.ru>
2024-05-27 12:14:17 +0400

    0. Принцип подстановки Лисков. [#4915] - примеры

---
+++ src/main/java/ru/job4j/ood/lsp/PhoneNumber.java
@@ -0,0 +1,37 @@
+package ru.job4j.ood.lsp;
+
+class PhoneNumber {
+
+    private int countryCode;
+
+    private int cityCode;
+
+    private int number;
+
+    public PhoneNumber(int countryCode, int cityCode, int number) {
+        this.countryCode = countryCode;
+        this.cityCode = cityCode;
+        this.number = number;
+    }
+
+    public int getCountryCode() {
+        return countryCode;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneNumber{"
+                + "countryCode=" + countryCode
+                + ", cityCode=" + cityCode
+                + ", number=" + number
+                + '}';
+    }
+}
