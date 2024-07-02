01bff120636b9affa000d50c279ded01d695c87b
YuryGe <postbox177@yandex.ru>
2024-07-02 21:11:32 +0400

    1. Динамическое перераспределение продуктов - дополнение

--- src/main/java/ru/job4j/ood/foodstore/control/ControlQuality.java
+++ src/main/java/ru/job4j/ood/foodstore/control/ControlQuality.java
@@ -1,7 +1,10 @@
 package ru.job4j.ood.foodstore.control;
 
 import ru.job4j.ood.foodstore.model.Food;
+import ru.job4j.ood.foodstore.store.Store;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ControlQuality {
@@ -13,5 +16,16 @@ public class ControlQuality {
 
     public void control(Food food) {
         processingRule.accept(food);
+    }
+
+    public void resort(List<Store> stores) {
+        List<Food> foods = new ArrayList<>();
+        stores.forEach(store -> store.getFoods().forEach(food -> {
+                            foods.add(food);
+                            store.removeFood(food);
+                        }
+                )
+        );
+        foods.forEach(this::control);
     }
 }
--- src/test/java/ru/job4j/ood/foodstore/control/ControlQualityTest.java
+++ src/test/java/ru/job4j/ood/foodstore/control/ControlQualityTest.java
@@ -38,7 +38,7 @@ class ControlQualityTest {
         ControlQuality controlQuality = new ControlQuality(food -> {
             long validityPeriod = food.getExpiryDate().getTimeInMillis()
                     - food.getCreateDate().getTimeInMillis();
-            long usedValidityPeriod = System.currentTimeMillis()
+            long usedValidityPeriod = new GregorianCalendar(2024, Calendar.JUNE, 1).getTimeInMillis()
                     - food.getCreateDate().getTimeInMillis();
             long consumptionOfValidityPeriod = validityPeriod > 0 && usedValidityPeriod > 0
                     ? usedValidityPeriod * 100 / validityPeriod : 100;
@@ -56,6 +56,66 @@ class ControlQualityTest {
 
         List<Food> foods = List.of(lollipop, rice, meat, fish, tomato, cheese);
         foods.forEach(controlQuality::control);
+
+        assertThat(warehouse.getFoods()).hasSameElementsAs(Set.of(lollipop, rice));
+
+        assertThat(shop.getFoods()).hasSameElementsAs(Set.of(meat, fish, tomato));
+        assertThat(fish.getPrice()).isCloseTo(8, offset(0.1f));
+        assertThat(tomato.getPrice()).isCloseTo(16, offset(0.1f));
+
+        assertThat(trash.getFoods()).hasSameElementsAs(Set.of(cheese));
+    }
+
+    @Test
+    void whenResortStores() {
+        Food lollipop = new Lollipop("Lollipops", new GregorianCalendar(2024, Calendar.JANUARY, 1),
+                new GregorianCalendar(2034, Calendar.DECEMBER, 31));
+        Food rice = new Rice("Rice", new GregorianCalendar(2024, Calendar.JANUARY, 1),
+                new GregorianCalendar(2034, Calendar.DECEMBER, 31));
+        Food meat = new Meat("Meat", new GregorianCalendar(2024, Calendar.JANUARY, 1),
+                new GregorianCalendar(2024, Calendar.DECEMBER, 31));
+        Food fish = new Fish("Fish", new GregorianCalendar(2024, Calendar.JANUARY, 1),
+                new GregorianCalendar(2024, Calendar.JUNE, 30));
+        fish.setPrice(10f);
+        Food tomato = new Tomato("Tomato", new GregorianCalendar(2024, Calendar.JANUARY, 1),
+                new GregorianCalendar(2024, Calendar.JUNE, 30));
+        tomato.setPrice(20f);
+        Food cheese = new Cheese("Cheese", new GregorianCalendar(2024, Calendar.JANUARY, 1),
+                new GregorianCalendar(2024, Calendar.MAY, 31));
+
+        Store warehouse = new Warehouse();
+        Store shop = new Shop();
+        Store trash = new Trash();
+
+        ControlQuality controlQuality = new ControlQuality(food -> {
+            long validityPeriod = food.getExpiryDate().getTimeInMillis()
+                    - food.getCreateDate().getTimeInMillis();
+            long usedValidityPeriod = new GregorianCalendar(2024, Calendar.JUNE, 1).getTimeInMillis()
+                    - food.getCreateDate().getTimeInMillis();
+            long consumptionOfValidityPeriod = validityPeriod > 0 && usedValidityPeriod > 0
+                    ? usedValidityPeriod * 100 / validityPeriod : 100;
+            if (consumptionOfValidityPeriod < 25) {
+                warehouse.addFood(food);
+            } else if (consumptionOfValidityPeriod <= 75) {
+                shop.addFood(food);
+            } else if (consumptionOfValidityPeriod < 100) {
+                shop.addFood(food);
+                food.setPrice(food.getPrice() * 0.8f);
+            } else {
+                trash.addFood(food);
+            }
+        });
+
+        warehouse.addFood(lollipop);
+        warehouse.addFood(rice);
+        warehouse.addFood(meat);
+
+        shop.addFood(fish);
+
+        trash.addFood(tomato);
+        trash.addFood(cheese);
+
+        controlQuality.resort(List.of(warehouse, shop, trash));
 
         assertThat(warehouse.getFoods()).hasSameElementsAs(Set.of(lollipop, rice));
 
